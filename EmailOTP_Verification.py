90546ea6b16bfdc8838e5b3d6d4aa0c0bb3f74e1
Siddhant Mahajani <sidhantmahajani@gmail.com>
2021-05-17 13:58:36 +0530

    EmailOTP_Verification.py
    Created main py file for OTP verification

---
+++ EmailOTP_Verification.py
@@ -0,0 +1,29 @@
+import os
+import math
+import random
+import smtplib
+from email.mime.text import MIMEText
+
+digits = "0123456789"
+
+OTP = ""
+for i in range(6):
+    OTP += digits[math.floor(random.random() * 10)]
+
+fromEmail = '{fromEmail}'
+
+mailServer = smtplib.SMTP('{host}', '{port}')
+mailServer.starttls()
+mailServer.login('{authenticationId}', '{authenticationPassword}')
+toEmail = input('Please enter your email: ')
+otpMimeMessage = MIMEText(OTP + " is your One Time Password. Please do not share it with anyone.")
+otpMimeMessage['Subject'] = 'OTP Verification'
+otpMimeMessage['From'] = fromEmail
+otpMimeMessage['To'] = toEmail
+mailServer.sendmail(fromEmail, toEmail, otpMimeMessage.as_string())
+
+otp = input('Enter your OTP: ')
+if otp == OTP:
+    print('Verification successful!')
+else:
+    print("Invalid OTP. Please enter valid OTP.")
